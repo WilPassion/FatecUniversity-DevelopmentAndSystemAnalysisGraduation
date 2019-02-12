71a15f5e863b35575e8bbedfc81358e211926b11
wilpassion <32945521+wilpassion@users.noreply.github.com>
2019-02-12 01:42:23 -0200

    University Exercises

---
+++ 1Semester/Microcomputer_Programming/Simple_Calculador/Exerc1Calculadora/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Exerc1Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1_textbox = new System.Windows.Forms.TextBox();
+            this.num2_textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbxOper = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // num1_textbox
+            // 
+            this.num1_textbox.Location = new System.Drawing.Point(154, 95);
+            this.num1_textbox.Name = "num1_textbox";
+            this.num1_textbox.Size = new System.Drawing.Size(209, 23);
+            this.num1_textbox.TabIndex = 1;
+            // 
+            // num2_textbox
+            // 
+            this.num2_textbox.Location = new System.Drawing.Point(154, 140);
+            this.num2_textbox.Name = "num2_textbox";
+            this.num2_textbox.Size = new System.Drawing.Size(209, 23);
+            this.num2_textbox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Valor1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(79, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Valor2";
+            // 
+            // cbxOper
+            // 
+            this.cbxOper.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxOper.FormattingEnabled = true;
+            this.cbxOper.Items.AddRange(new object[] {
+            "Soma",
+            "Subtração",
+            "Multiplicação",
+            "Divisão",
+            "Potência"});
+            this.cbxOper.Location = new System.Drawing.Point(153, 212);
+            this.cbxOper.Name = "cbxOper";
+            this.cbxOper.Size = new System.Drawing.Size(209, 25);
+            this.cbxOper.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(203, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Operação";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(32, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 36);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(351, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 36);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(192, 311);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 36);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Limpar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(204, 257);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(110, 25);
+            this.lblResult.TabIndex = 11;
+            this.lblResult.Text = "Resultado: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 429);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxOper);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.num2_textbox);
+            this.Controls.Add(this.num1_textbox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox num1_textbox;
+        private System.Windows.Forms.TextBox num2_textbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbxOper;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ 1Semester/Microcomputer_Programming/Simple_Calculador/Exerc1Calculadora/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exerc1Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1;
+            double num2;
+            double result = 0;
+            int oper;
+
+            num1 = (Convert.ToInt32(num1_textbox.Text));
+            num2 = (Convert.ToInt32(num2_textbox.Text));
+
+            oper = (Convert.ToInt32(cbxOper.SelectedIndex));
+
+            if (oper == -1)
+            {
+                MessageBox.Show("Operação inválida", "Calculadora", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            if (oper == 0)
+            {
+                result = num1 + num2;                
+            }
+            else 
+            if(oper == 1)
+            {
+                result = num1 - num2;                
+            }
+            else
+            if (oper == 2)
+            {
+                result = num1 * num2;                
+            }
+            else
+            if (oper == 3)
+            {
+                result = num1 / num2;                
+            }
+            else
+            if (oper == 4)
+            {
+                result = Math.Pow(num1, num2);                
+            }
+            lblResult.Text = ("Resultado : " + result);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            num1_textbox.Text = "";
+            num2_textbox.Text = "";
+            lblResult.Text = "Resultado: ";
+            cbxOper.SelectedIndex = -1;
+
+            lblResult.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
